0fbf8dff96df28e710320bc3e95a9025abeeaffe
Nick Anderson <njcanderson@gmail.com>
2018-09-12 21:32:29 -0400

    removed read option so that 'git bookmark' reads the current bookmark

--- git-bookmark.py
+++ git-bookmark.py
@@ -1,14 +1,14 @@
 import sys
 import os
 
-if 'read' in sys.argv:
+if len(sys.argv) == 1:
     if not os.path.exists('.git'):
         print('fatal: Not a git repository: no .git directory')
         sys.exit(0)
     with open('.git/bookmark', 'r') as file:
         print(file.read())
 elif len(sys.argv) != 2:
-    print('Usage: git bookmark read || git bookmark \"Message to note your current progress\"')
+    print('Usage: git bookmark || git bookmark \"Message to note your current progress\"')
 else:
     if not os.path.exists('.git'):
         print('fatal: Not a git repository: no .git directory')
